c632cf568325e92fc0d287b1d5ed86fdc8892f47
gauravch45 <gauravch45@gmail.com>
2025-03-12 00:33:07 +0530

    Added process to console.log REACT_APP_API_URL

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -2,8 +2,17 @@ import TaskList from './components/TaskList';
 //import TaskForm from './components/TaskForm';
 import './App.css';
 
+import React, { useEffect } from 'react';
+
+
 
 function App() {
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_URL);
+  }, []);
+
+  
   return (
     <div className="App">
       <h1>Task Manager</h1>
